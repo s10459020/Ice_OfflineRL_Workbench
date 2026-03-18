c90813b6750d42e7d0a6787ae7a526c149ea6bf7
Ice <bluewindice2@gmail.com>
2026-03-18 15:43:31 +0800

    start vscode test system

--- source/tools/__init__.py
+++ source/tools/__init__.py
@@ -1,7 +1,7 @@
 from .env_dumper_wrapper import EnvDumper, EnvDumperWrapper, run_env_dump
 from .render_quite_wrapper import RenderQuiteWrapper, ensure_render_quite
 from .step_penalty_wrapper import StepPenaltyWrapper
-from .test_format import print_banner, stage
+from .print_format import print_banner, stage
 
 __all__ = [
     "EnvDumperWrapper",
